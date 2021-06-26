3053f881c29952950f16efc422386d0ad3d20891
Muhammad Usman <muhammadusman1.me@gmail.com>
2021-06-26 20:42:25 +0500

    Clean for windows usages only

--- autotimer.py
+++ autotimer.py
@@ -23,16 +23,8 @@ def url_to_name(url):
 
 def get_active_window():
     _active_window_name = None
-    if sys.platform in ['Windows', 'win32', 'cygwin']:
-        window = win32gui.GetForegroundWindow()
-        _active_window_name = win32gui.GetWindowText(window)
-    elif sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
-        _active_window_name = (NSWorkspace.sharedWorkspace()
-                               .activeApplication()['NSApplicationName'])
-    else:
-        print("sys.platform={platform} is not supported."
-              .format(platform=sys.platform))
-        print(sys.version)
+    window = win32gui.GetForegroundWindow()
+    _active_window_name = win32gui.GetWindowText(window)
     return _active_window_name
 
 
